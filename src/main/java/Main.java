05b6b73ace4113ea9a130be29a1d8cb647653728
olexandr <kutoviyo@gmail.com>
2017-05-09 14:07:30 +0300

    Change SQLite to SQLite3 DB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,30 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
         Connection c = null;
+        Statement stmt = null;
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:attendance.db");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "SELECT * FROM temps;";
+            ResultSet rs =stmt.executeQuery(sql);
+            while ( rs.next() ) {
+                System.out.println( "RS = " + rs.toString() );
+            }
+            rs.close();
+            stmt.close();
+            c.close();
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
     }
 }
