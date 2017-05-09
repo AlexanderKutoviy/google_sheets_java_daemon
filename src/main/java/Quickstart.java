d0e22cc9a74b0fd15de9bddee615036cc18c0145
olexandr <kutoviyo@gmail.com>
2017-05-09 19:29:21 +0300

    Attempt to update cells values

--- src/main/java/Quickstart.java
+++ src/main/java/Quickstart.java
@@ -16,6 +16,7 @@ import com.google.api.services.sheets.v4.Sheets;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,11 +24,11 @@ public class Quickstart {
 
     private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
     private static final java.io.File DATA_STORE_DIR = new java.io.File(
-            System.getProperty("user.home"), ".credentials/sheets.googleapis.com-java-quickstart");
+            System.getProperty("user.home"), ".credentials/sheets.googleapis.com-java-quickstart1");
     private static FileDataStoreFactory DATA_STORE_FACTORY;
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static HttpTransport HTTP_TRANSPORT;
-    private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS_READONLY);
+    private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS, SheetsScopes.DRIVE);
 
     static {
         try {
@@ -49,6 +50,7 @@ public class Quickstart {
                 new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                         .setDataStoreFactory(DATA_STORE_FACTORY)
                         .setAccessType("offline")
+                        .setApprovalPrompt("auto")
                         .build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
         System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
@@ -67,19 +69,41 @@ public class Quickstart {
         Sheets service = getSheetsService();
 
 //        https://docs.google.com/spreadsheets/d/10tQ5MHGGiDcoqTdEV2Pupy6wh171wyRbjSH-eXbaKC8/edit?usp=sharing
+//        https://docs.google.com/spreadsheets/d/10tQ5MHGGiDcoqTdEV2Pupy6wh171wyRbjSH-eXbaKC8/edit#gid=0
         String spreadsheetId = "10tQ5MHGGiDcoqTdEV2Pupy6wh171wyRbjSH-eXbaKC8";
-        String range = "A2:F";
-        ValueRange response = service.spreadsheets().values()
-                .get(spreadsheetId, range)
-                .execute();
-        List<List<Object>> values = response.getValues();
-        if (values == null || values.size() == 0) {
-            System.out.println("No data found.");
-        } else {
-            for (List row : values) {
-                System.out.printf("%s, %s, %s, %s, %s, %s\n", row.get(0), row.get(1), row.get(2), row.get(3), row.get(4), row.get(5));
-            }
-        }
+        String range = "B3";
+//        ValueRange response = service.spreadsheets().values()
+//                .get(spreadsheetId, range)
+//                .execute();
+//        List<List<Object>> values = response.getValues();
+//        if (values == null || values.size() == 0) {
+//            System.out.println("No data found.");
+//        } else {
+//            for (List row : values) {
+//                System.out.printf("%s, %s, %s, %s, %s, %s\n", row.get(0), row.get(1), row.get(2), row.get(3), row.get(4), row.get(5));
+//            }
+//        }
+        List<Request> requests = new ArrayList<Request>();
+// Change the spreadsheet's title.
+        requests.add(new Request()
+                .setUpdateSpreadsheetProperties(new UpdateSpreadsheetPropertiesRequest()
+                        .setProperties(new SpreadsheetProperties()
+                                .setTitle("KEK_SHEET"))
+                        .setFields("title")));
+// Find and replace text.
+        requests.add(new Request()
+                .setFindReplace(new FindReplaceRequest()
+                        .setFind("KEK")
+                        .setReplacement("LOL")
+                        .setAllSheets(true)));
+// Add additional requests (operations) ...
+
+        BatchUpdateSpreadsheetRequest body =
+                new BatchUpdateSpreadsheetRequest().setRequests(requests);
+        BatchUpdateSpreadsheetResponse response =
+                service.spreadsheets().batchUpdate(spreadsheetId, body).execute();
+        FindReplaceResponse findReplaceResponse = response.getReplies().get(1).getFindReplace();
+        System.out.printf("%d replacements made.", findReplaceResponse.getOccurrencesChanged());
     }
 
 
